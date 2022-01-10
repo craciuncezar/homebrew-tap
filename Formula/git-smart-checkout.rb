d9fb417a5ac3a0dca325f9fee35a19fbb08db243
Cezar Craciun <craciuncezar1996@gmail.com>
2022-01-10 22:09:14 +0200

    Update git-smart-checkout.rb

--- Formula/git-smart-checkout.rb
+++ Formula/git-smart-checkout.rb
@@ -4,7 +4,7 @@ class GitSmartCheckout < Formula
     url "https://github.com/craciuncezar/git-smart-checkout/releases/download/v0.1.2/git-smart-checkout.tar.gz"
     sha256 "53f4370332847ac943f57dd2e401d437a2cdf9bbadec6acc29d71e77c127790a"
     license "MIT"
-    version "0.1.0"
+    version "0.1.2"
     
     def install
       bin.install "git-smart-checkout"
