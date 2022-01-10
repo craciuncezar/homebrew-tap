8699aed965a26e57709bfff0f6c78aaa98fb7f43
Cezar Craciun <craciuncezar1996@gmail.com>
2022-01-10 22:09:02 +0200

    Update git-smart-checkout.rb

--- Formula/git-smart-checkout.rb
+++ Formula/git-smart-checkout.rb
@@ -1,8 +1,8 @@
 class GitSmartCheckout < Formula
     desc "A git command extension for switching git branches more efficiently."
     homepage "https://github.com/craciuncezar/git-smart-checkout"
-    url "https://github.com/craciuncezar/git-smart-checkout/releases/download/v0.1.0/git-smart-checkout.tar.gz"
-    sha256 "81d1169cc14c86c1b33b7e9b3064c24ecf708da7a2a9438a2a400dac198213de"
+    url "https://github.com/craciuncezar/git-smart-checkout/releases/download/v0.1.2/git-smart-checkout.tar.gz"
+    sha256 "53f4370332847ac943f57dd2e401d437a2cdf9bbadec6acc29d71e77c127790a"
     license "MIT"
     version "0.1.0"
     
